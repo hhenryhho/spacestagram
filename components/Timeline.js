0ede7ce1c4cce2226b70ca8ccf1924af6dc01e03
Henry Ho <henryho73@hotmail.com>
2022-06-14 11:13:40 -0400

    Updated dates

--- components/Timeline.js
+++ components/Timeline.js
@@ -1,34 +1,48 @@
-import React, { useState, useEffect } from "react";
-import { Heading, Container, Stack, Spinner, Flex } from "@chakra-ui/react";
-import TimelineCard from "./TimelineCard";
+/** @format */
+
+import React, { useState, useEffect } from 'react'
+import {
+	Heading,
+	Container,
+	Stack,
+	Spinner,
+	Flex,
+} from '@chakra-ui/react'
+import TimelineCard from './TimelineCard'
 
 const Timeline = ({ posts }) => {
-  const [reversed, setReversed] = useState(false);
-  useEffect(() => {
-    setTimeout(() => {
-      posts = posts.reverse();
-      setReversed(!reversed);
-    }, 1000);
-  }, [posts]);
+	const [reversed, setReversed] = useState(false)
+	useEffect(() => {
+		setTimeout(() => {
+			posts = posts.reverse()
+			setReversed(!reversed)
+		}, 1000)
+	}, [posts])
 
-  return (
-    <Container>
-      <Stack>
-        <Heading pt={100} pb={2}>
-          Spacestagram
-        </Heading>
-        <Stack spacing={10} flexDir='column'>
-          {reversed ? (
-            posts.map((post, index) => <TimelineCard key={index} post={post} />)
-          ) : (
-            <Flex align='center' justify='center' w='100%' h='50vh'>
-              <Spinner />
-            </Flex>
-          )}
-        </Stack>
-      </Stack>
-    </Container>
-  );
-};
+	return (
+		<Container>
+			<Stack>
+				<Heading pt={100} pb={2}>
+					Spacestagram
+				</Heading>
+				<Stack spacing={10} flexDir='column'>
+					{reversed ? (
+						posts.map((post, index) => (
+							<TimelineCard key={index} post={post} />
+						))
+					) : (
+						<Flex
+							align='center'
+							justify='center'
+							w='100%'
+							h='50vh'>
+							<Spinner />
+						</Flex>
+					)}
+				</Stack>
+			</Stack>
+		</Container>
+	)
+}
 
-export default Timeline;
+export default Timeline
